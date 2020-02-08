87ee2a43c10aa3557fecb0d6be7a75c418007c34
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-02-08 18:49:11 +0700

    check valid

--- app/src/main/java/com/example/sharepreference26112019/MainActivity.java
+++ app/src/main/java/com/example/sharepreference26112019/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.sharepreference26112019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText mEdtUserName,mEdtPassword;
+    CheckBox mCbSave;
+    Button mBtnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapview();
+        setListener();
+    }
+
+    private void setListener() {
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = mEdtUserName.getText().toString();
+                String password = mEdtPassword.getText().toString();
+
+                if (username.equals("phatandroid") && password.equals("123456")){
+                    Toast.makeText(MainActivity.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Sai rồi", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void mapview() {
+        mEdtPassword = findViewById(R.id.edittextPassword);
+        mEdtUserName = findViewById(R.id.edittextUsername);
+        mCbSave = findViewById(R.id.checkboxSave);
+        mBtnLogin = findViewById(R.id.buttonLogin);
     }
 }
