fe3d5915695864021be6a75b025fa6b71d0d562e
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-02-08 19:06:24 +0700

    array set

--- app/src/main/java/com/example/sharepreference26112019/MainActivity.java
+++ app/src/main/java/com/example/sharepreference26112019/MainActivity.java
@@ -2,24 +2,43 @@ package com.example.sharepreference26112019;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText mEdtUserName,mEdtPassword;
     CheckBox mCbSave;
     Button mBtnLogin;
+    SharedPreferences mSharedPreferences;
+    SharedPreferences.Editor mEditor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
         mapview();
         setListener();
+    }
+
+    private void init() {
+        mSharedPreferences = getSharedPreferences("CacheApp",MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
+        Set<String> arrayName = new HashSet<>();
+        arrayName.add("phat");
+        arrayName.add("phat");
+        arrayName.add("phat");
+        arrayName.add("phat");
+        Log.d("BBB",arrayName.toString());
     }
 
     private void setListener() {
@@ -31,6 +50,10 @@ public class MainActivity extends AppCompatActivity {
 
                 if (username.equals("phatandroid") && password.equals("123456")){
                     Toast.makeText(MainActivity.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+//                    if (mCbSave.isChecked()){
+//                        mEditor.put
+//                    }
+
                 }else{
                     Toast.makeText(MainActivity.this, "Sai rồi", Toast.LENGTH_SHORT).show();
                 }
